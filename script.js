8297b06db05220828688d68d182b446c2955fa7e
BaileyB227 <87950291+BaileyB227@users.noreply.github.com>
2021-08-11 22:21:54 -0500

    got colored boxes for time working

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 let today = moment().format('dddd, MMMM Do');
+let currentTime = moment().hours();
+let timeIds = $(".row");
 
 $("#currentDay").text(today);
+
+for (var i = 0 ; i < timeIds.length ; i++) {
+    var elementID = timeIds[i].id;
+
+    var changedID = document.getElementById(timeIds[i].id)
+
+    if (elementID > currentTime) {
+        $(changedID).addClass("future");
+    } else if (elementID < currentTime) {
+        $(changedID).addClass("past");
+    } else {
+        $(changedID).addClass("present");
+    }
+}
